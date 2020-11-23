03b8515f325b114e62582254bbea68e010c3fb9e
Raf Dul <rafalduluk@gmail.com>
2020-11-23 16:20:12 +0100

    Add date range picker on the page_1_general_static

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+/* global $ */
+
+
+// datepicker
+
+$(function() {
+  $('input[name="dates"]').daterangepicker({
+    opens: 'left'
+  }, function(start, end, label) {
+    console.log('A new date selection was made: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+  });
+});
