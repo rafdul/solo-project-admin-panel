0eb5f452633a369b489df53e848b4c6afcd16c70
rafdul <rafalduluk@gmail.com>
2020-11-26 17:12:58 +0100

    Remove id in src

--- src/js/script.js
+++ src/js/script.js
@@ -6,7 +6,7 @@
 $(function() {
   $('input[name="dates"]').daterangepicker({
     opens: 'left'
-  }, function(start, end, label) {
+  }, function(start, end) {
     console.log('A new date selection was made: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
   });
 });
