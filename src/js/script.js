73048ef96b7d43778a07b09a2836f8dd28f84c0a
rafdul <rafalduluk@gmail.com>
2020-11-30 19:38:58 +0100

    Add validation of form in javascript

--- src/js/script.js
+++ src/js/script.js
@@ -10,3 +10,50 @@ $(function() {
     console.log('A new date selection was made: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
   });
 });
+
+
+// walidacja formularza
+console.log(document.querySelector('form'));
+
+document.querySelector('form').addEventListener('submit', function(event) {
+  var isFormValidate = true;
+
+  // walidacja pola Name
+  var nameInput = event.target.querySelector('input[name="user-name"]');
+  if(nameInput.value === '') {
+    isFormValidate = false;
+    nameInput.parentElement.querySelector('.error').innerHTML = 'Pole "Name" jest wymagane';
+  }
+
+  // walidacja email
+  var emailAddressInput = event.target.querySelector('input[name="user-email"]');
+  const regEmail = /[a-zA-Z_0-9-]{3,}@[a-zA-Z0-9-]{2,}[.]{1}[a-zA-Z]{2,}/;
+  if(regEmail.test(emailAddressInput.value) == false) {
+    isFormValidate = false;
+    emailAddressInput.parentElement.querySelector('.error').innerHTML = 'Błędny adres e-mail';
+  }
+
+  // walidacja tel
+  var telAddressInput = event.target.querySelector('input[name="user-phone"]');
+  const regTel = /[0-9]{3}-[0-9]{3}-[0-9]{3}/;
+  if(regTel.test(telAddressInput.value) == false) {
+    isFormValidate = false;
+    telAddressInput.parentElement.querySelector('.error').innerHTML = 'Błędny numer telefonu';
+  }
+
+  // walidacja password i repeat
+  var passwordInput = event.target.querySelector('input[name="user-password"]');
+  var repeatInput = event.target.querySelector('input[name="user-repeat"]');
+  const regPassword = /[\S]{5,}/;
+  if(regPassword.test(passwordInput.value) == false) {
+    isFormValidate = false;
+    passwordInput.parentElement.querySelector('.error').innerHTML = 'Hasło musi mieć min. 5 znaków';
+  }
+  if (repeatInput.value != passwordInput.value) {
+    isFormValidate = false;
+    repeatInput.parentElement.querySelector('.error').innerHTML = 'Powtórz poprawne hasło';
+  }
+
+  return !isFormValidate ? event.preventDefault() : true;
+});
+
