8dbc2aade21ffa5439ffdfc95823bcc97a335120
rafdul <rafalduluk@gmail.com>
2020-11-30 21:55:50 +0100

    Add comments in validation of form

--- src/js/script.js
+++ src/js/script.js
@@ -12,7 +12,7 @@ $(function() {
 });
 
 
-// walidacja formularza
+// walidacja formularza po kliknięciu w submit
 console.log(document.querySelector('form'));
 
 document.querySelector('form').addEventListener('submit', function(event) {
@@ -22,7 +22,9 @@ document.querySelector('form').addEventListener('submit', function(event) {
   var nameInput = event.target.querySelector('input[name="user-name"]');
   if(nameInput.value === '') {
     isFormValidate = false;
-    nameInput.parentElement.querySelector('.error').innerHTML = 'Pole "Name" jest wymagane';
+    nameInput.parentElement.parentElement.querySelector('.error').innerHTML = 'Pole "Name" jest wymagane';
+    nameInput.parentElement.classList.add('invalid');
+    console.log(nameInput.parentElement);
   }
 
   // walidacja email
@@ -30,7 +32,8 @@ document.querySelector('form').addEventListener('submit', function(event) {
   const regEmail = /[a-zA-Z_0-9-]{3,}@[a-zA-Z0-9-]{2,}[.]{1}[a-zA-Z]{2,}/;
   if(regEmail.test(emailAddressInput.value) == false) {
     isFormValidate = false;
-    emailAddressInput.parentElement.querySelector('.error').innerHTML = 'Błędny adres e-mail';
+    emailAddressInput.parentElement.parentElement.querySelector('.error').innerHTML = 'Błędny adres e-mail';
+    emailAddressInput.parentElement.classList.add('invalid');
   }
 
   // walidacja tel
@@ -38,7 +41,8 @@ document.querySelector('form').addEventListener('submit', function(event) {
   const regTel = /[0-9]{3}-[0-9]{3}-[0-9]{3}/;
   if(regTel.test(telAddressInput.value) == false) {
     isFormValidate = false;
-    telAddressInput.parentElement.querySelector('.error').innerHTML = 'Błędny numer telefonu';
+    telAddressInput.parentElement.parentElement.querySelector('.error').innerHTML = 'Błędny numer telefonu';
+    telAddressInput.parentElement.classList.add('invalid');
   }
 
   // walidacja password i repeat
@@ -47,11 +51,13 @@ document.querySelector('form').addEventListener('submit', function(event) {
   const regPassword = /[\S]{5,}/;
   if(regPassword.test(passwordInput.value) == false) {
     isFormValidate = false;
-    passwordInput.parentElement.querySelector('.error').innerHTML = 'Hasło musi mieć min. 5 znaków';
+    passwordInput.parentElement.parentElement.querySelector('.error').innerHTML = 'Hasło musi mieć min. 5 znaków';
+    passwordInput.parentElement.classList.add('invalid');
   }
   if (repeatInput.value != passwordInput.value) {
     isFormValidate = false;
-    repeatInput.parentElement.querySelector('.error').innerHTML = 'Powtórz poprawne hasło';
+    repeatInput.parentElement.parentElement.querySelector('.error').innerHTML = 'Powtórz poprawnie swoje hasło';
+    repeatInput.parentElement.classList.add('invalid');
   }
 
   return !isFormValidate ? event.preventDefault() : true;
