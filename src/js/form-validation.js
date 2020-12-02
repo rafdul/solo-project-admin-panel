779b64b8e31ba563c6bb5694e4bbc38b71321646
rafdul <rafalduluk@gmail.com>
2020-12-02 13:14:39 +0100

    Show and hide side-menu in javascript

---
+++ src/js/form-validation.js
@@ -0,0 +1,50 @@
+// walidacja formularza po kliknięciu w submit
+// console.log(document.querySelector('form'));
+
+document.querySelector('form').addEventListener('submit', function(event) {
+  var isFormValidate = true;
+
+  // walidacja pola Name
+  var nameInput = event.target.querySelector('input[name="user-name"]');
+  if(nameInput.value === '') {
+    isFormValidate = false;
+    nameInput.parentElement.parentElement.querySelector('.error').innerHTML = 'Pole "Name" jest wymagane';
+    nameInput.parentElement.classList.add('invalid');
+    console.log(nameInput.parentElement);
+  }
+
+  // walidacja email
+  var emailAddressInput = event.target.querySelector('input[name="user-email"]');
+  const regEmail = /[a-zA-Z_0-9-]{3,}@[a-zA-Z0-9-]{2,}[.]{1}[a-zA-Z]{2,}/;
+  if(regEmail.test(emailAddressInput.value) == false) {
+    isFormValidate = false;
+    emailAddressInput.parentElement.parentElement.querySelector('.error').innerHTML = 'Błędny adres e-mail';
+    emailAddressInput.parentElement.classList.add('invalid');
+  }
+
+  // walidacja tel
+  var telAddressInput = event.target.querySelector('input[name="user-phone"]');
+  const regTel = /[0-9]{3}-[0-9]{3}-[0-9]{3}/;
+  if(regTel.test(telAddressInput.value) == false) {
+    isFormValidate = false;
+    telAddressInput.parentElement.parentElement.querySelector('.error').innerHTML = 'Błędny numer telefonu';
+    telAddressInput.parentElement.classList.add('invalid');
+  }
+
+  // walidacja password i repeat
+  var passwordInput = event.target.querySelector('input[name="user-password"]');
+  var repeatInput = event.target.querySelector('input[name="user-repeat"]');
+  const regPassword = /[\S]{5,}/;
+  if(regPassword.test(passwordInput.value) == false) {
+    isFormValidate = false;
+    passwordInput.parentElement.parentElement.querySelector('.error').innerHTML = 'Hasło musi mieć min. 5 znaków';
+    passwordInput.parentElement.classList.add('invalid');
+  }
+  if (repeatInput.value != passwordInput.value) {
+    isFormValidate = false;
+    repeatInput.parentElement.parentElement.querySelector('.error').innerHTML = 'Powtórz poprawnie swoje hasło';
+    repeatInput.parentElement.classList.add('invalid');
+  }
+
+  return !isFormValidate ? event.preventDefault() : true;
+});
--- src/js/script.js
+++ src/js/script.js
@@ -10,24 +10,7 @@ $(function() {
   });
 });
 
-// wysuwanie menu bocznego
-// sprawdzenie szerokości ekranu i nadanie nowej klasy (w określonej sytuacji)
-
-var mobileViewport = window.matchMedia('screen and (max-width: 500px)');
-
-mobileViewport.addEventListener('change', function(mq) {
-  if(mq.matches == true) {
-    if(document.querySelector('.dashboard').classList.contains('min-grid') == false) {
-      document.querySelector('.dashboard').classList.add('min-grid');
-    }
-  }
-});
-
-// console.log(document.querySelector('.dashboard'));
-// console.log(document.querySelector('.dashboard').style.getPropertyValue('grid-template-columns'));
-
-
-// nadanie nowej klasy poprzez klik w hamburger
+// nadanie klasy min-grid poprzez klik w hamburger
 console.log(document.querySelector('.hamburger'));
 document.querySelector('.hamburger').addEventListener('click', function(event) {
   event.preventDefault();
@@ -35,56 +18,33 @@ document.querySelector('.hamburger').addEventListener('click', function(event) {
   console.log(document.querySelector('.dashboard'));
 });
 
+// wysuwanie menu bocznego
+// sprawdzenie szerokości ekranu i nadanie klasy min-grid (w określonej sytuacji)
 
+let mobileViewport = window.matchMedia('screen and (max-width: 500px)');
 
-// walidacja formularza po kliknięciu w submit
-// console.log(document.querySelector('form'));
+mobileViewport.addEventListener('change', function(mq) {
+  if(mq.matches == true) {
+    changeDisplay();
 
-document.querySelector('form').addEventListener('submit', function(event) {
-  var isFormValidate = true;
-
-  // walidacja pola Name
-  var nameInput = event.target.querySelector('input[name="user-name"]');
-  if(nameInput.value === '') {
-    isFormValidate = false;
-    nameInput.parentElement.parentElement.querySelector('.error').innerHTML = 'Pole "Name" jest wymagane';
-    nameInput.parentElement.classList.add('invalid');
-    console.log(nameInput.parentElement);
+    if(document.querySelector('.dashboard').classList.contains('min-grid') == false) {
+      document.querySelector('.dashboard').classList.add('min-grid');
+    }
   }
-
-  // walidacja email
-  var emailAddressInput = event.target.querySelector('input[name="user-email"]');
-  const regEmail = /[a-zA-Z_0-9-]{3,}@[a-zA-Z0-9-]{2,}[.]{1}[a-zA-Z]{2,}/;
-  if(regEmail.test(emailAddressInput.value) == false) {
-    isFormValidate = false;
-    emailAddressInput.parentElement.parentElement.querySelector('.error').innerHTML = 'Błędny adres e-mail';
-    emailAddressInput.parentElement.classList.add('invalid');
-  }
-
-  // walidacja tel
-  var telAddressInput = event.target.querySelector('input[name="user-phone"]');
-  const regTel = /[0-9]{3}-[0-9]{3}-[0-9]{3}/;
-  if(regTel.test(telAddressInput.value) == false) {
-    isFormValidate = false;
-    telAddressInput.parentElement.parentElement.querySelector('.error').innerHTML = 'Błędny numer telefonu';
-    telAddressInput.parentElement.classList.add('invalid');
-  }
-
-  // walidacja password i repeat
-  var passwordInput = event.target.querySelector('input[name="user-password"]');
-  var repeatInput = event.target.querySelector('input[name="user-repeat"]');
-  const regPassword = /[\S]{5,}/;
-  if(regPassword.test(passwordInput.value) == false) {
-    isFormValidate = false;
-    passwordInput.parentElement.parentElement.querySelector('.error').innerHTML = 'Hasło musi mieć min. 5 znaków';
-    passwordInput.parentElement.classList.add('invalid');
-  }
-  if (repeatInput.value != passwordInput.value) {
-    isFormValidate = false;
-    repeatInput.parentElement.parentElement.querySelector('.error').innerHTML = 'Powtórz poprawnie swoje hasło';
-    repeatInput.parentElement.classList.add('invalid');
-  }
-
-  return !isFormValidate ? event.preventDefault() : true;
 });
+
+// zmiana z display none na block dla elementów top-menu w side-menu
+function changeDisplay() {
+  const element = document.querySelector('.side-menu__top-menu');
+  console.log(element.style.display);
+  if(element.style.display == 'none' || element.style.display == '') {
+    element.style.display = 'block';
+  }
+  console.log(element.style.display);
+}
+
+
+
+
+
 
