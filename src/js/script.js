b323f6275fa9b6c601d6c88a925776666c612958
rafdul <rafalduluk@gmail.com>
2020-12-05 19:23:11 +0100

    Add plugin dataTable

--- src/js/script.js
+++ src/js/script.js
@@ -8,3 +8,24 @@ $(function() {
     console.log('A new date selection was made: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
   });
 });
+
+// datatables - stronicowanie i sortowanie tabeli
+$(document).ready( function () {
+  $('#table-payout').DataTable({
+    'language': {
+      'url': '//cdn.datatables.net/plug-ins/1.10.21/i18n/Polish.json'
+    },
+    'columnDefs': [{
+      'width': '23%',
+    }]
+  });
+});
+
+
+var table = $('#table-payout').DataTable();
+$('#table-payout').css('display', 'table');
+// table.responsive.recalc();
+
+// $('#table-payout').DataTable()
+//   .columns.adjust()
+//   .responsive.recalc();
